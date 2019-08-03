e7fc1ab7b4517f7eb306137c9c33f1ffdc15659c
Fiqri Hafzain <fiqrihafzainislami@gmail.com>
2019-08-03 11:55:29 +0700

    fixing

--- app/src/main/java/com/syntax/tutorialviewmodel/MainActivity.kt
+++ app/src/main/java/com/syntax/tutorialviewmodel/MainActivity.kt
@@ -2,11 +2,51 @@ package com.syntax.tutorialviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.syntax.tutorialviewmodel.model.Result
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    // deklarasi ViewModel
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * communication your activity/fragment with MainViewModel
+         * */
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        /**
+         * get value from your live data into MainViewModel
+         * */
+        viewModel.getHitung().observe(this, getHitung) // => asynchronous
+
+        btn_hitung.setOnClickListener {
+            hitung(edt_panjang.text.toString().trim(), edt_panjang.text.toString().trim())
+        }
+    }
+
+    fun hitung(panjang: String, lebar: String) {
+        if (panjang.isEmpty() || lebar.isEmpty()) {
+            toast("kosong cuy")
+        } else {
+            viewModel.setHitung(edt_panjang.text.toString().trim(), edt_lebar.text.toString().trim())
+        }
+    }
+
+    /**
+     * SUBSCRIBE
+     * call this if you want get your data when function gethitung() to do observe
+     * */
+    val getHitung = Observer<Result> {
+        it.let {
+            tv_result.text = it?.result
+        }
     }
 }
--- app/src/main/java/com/syntax/tutorialviewmodel/MainViewModel.kt
+++ app/src/main/java/com/syntax/tutorialviewmodel/MainViewModel.kt
@@ -1,6 +1,38 @@
 package com.syntax.tutorialviewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.syntax.tutorialviewmodel.model.Result
 
 class MainViewModel : ViewModel() {
+
+    val liveData = MutableLiveData<Result>() // => can change value
+
+    fun setHitung(panjang: String, lebar: String): LiveData<Result> { // => LiveData is Read Only
+        val p = panjang.toDouble()
+        val l = lebar.toDouble()
+        val tampung = p * l
+        val result = Result(tampung.toString())
+        liveData.postValue(result)
+        return liveData
+    }
+
+    /**
+     * return value from liveData if you have done calculate
+     * */
+    fun getHitung(): LiveData<Result> {
+        return liveData
+    }
+
+    /**
+     *
+     * NOTED
+     * do not pass context into this class ViewModel
+     * if you want need context from this class => avoiding memory leaks
+     * you can implement like this :
+     *
+     * class MainViewModel(application: Application) : AndroidViewModel(application) {
+     * }
+     * */
 }
